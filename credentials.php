86afd08cd5d6d1d985bcab85bf0b4a64872cfffd
magsfragz <118135133+magsfragz@users.noreply.github.com>
2023-10-28 21:27:54 -0400

    Add files via upload

---
+++ credentials.php
@@ -0,0 +1,5 @@
+<?php
+$name = "valorantfull"; // Application name
+$OwnerId = "fU7MG3TIqT"; // Application ownerID
+$SellerKey = "174ee13d3c3b8796fda2887a5fc43256"; // application's seller key. Found in seller settings (You must have seller plan)
+?>
